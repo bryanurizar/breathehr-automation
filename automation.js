ba258877e92dce0738b1b0001725e1c411f33f94
Bryan Urizar <bryan.urizar@outlook.com>
2021-01-15 15:08:30 +0000

    loop through annual leave days added

--- automation.js
+++ automation.js
@@ -3,8 +3,6 @@ const puppeteer = require('puppeteer');
 const moment = require('moment-business-days');
 const leave = require('./leave');
 
-console.log(leave.annualLeave);
-
 const url = 'https://hr.breathehr.com/employees/524792/time_logs/new?identifier=healthcodeltd&log_return_to=dashboard';
 
 moment.updateLocale('us', {
@@ -16,36 +14,39 @@ moment.updateLocale('us', {
 const startDate = moment('01/01/2020', 'DD/MM/YYYY');
 const endDate = moment('31/12/2020', 'DD/MM/YYYY');
 
-for (let day = startDate; day <= endDate; day.add(1, 'd')) {
-    if (day.isHoliday(day) || leave.annualLeave.includes(day.format('DD/MM/YYYY'))) {
-        console.log("Bank Holiday/Leave Day")
-        console.log(day.format('DD/MM/YYYY'));
-        // logDay(day, "Leave", "LEAVE (inc Bank Holidays)");
-    } else {
-        console.log("Regular working day")
-        console.log(day.format('DD/MM/YYYY'));
-        // logDay(day, "Work", "General Support");
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await page.type('#email-input', process.env.EMAIL);
+    await page.type('.password-input', process.env.PASSWORD);
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation({ waitUntil: ['networkidle2'] });
+
+    for (let day = startDate; day <= endDate; day.add(1, 'd')) {
+        if (day.isHoliday(day) || leave.annualLeave.includes(day)) {
+            console.log(day)
+            await logDate(day.format("DD/MM/YYYY"), "Leave", "LEAVE (inc Bank Holidays)", page);
+        } else {
+            console.log("Regular working day")
+            console.log(day.format('DD/MM/YYYY'));
+            await logDate(day.format("DD/MM/YYYY"), "Work", "General Support", page);
+        }
     }
-}
+})();
 
-function logDay(date, description, type) {
-    (async () => {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.goto(url);
-
-        await page.type('#email-input', process.env.EMAIL);
-        await page.type('.password-input', process.env.PASSWORD);
-        await page.keyboard.press('Enter');
-
-        await page.waitForNavigation({ waitUntil: ['networkidle2'] });
-        await page.type('.DateInput_input', date);
+async function logDate(day, description, type, page) {
+    try {
+        await page.type('.DateInput_input', day);
         await page.type('#employee_time_log_description', description);
         await page.type('#employee_time_log_hours', '7');
         await page.type('#employee_time_log_company_project_id', type);
         await page.keyboard.press('Enter');
-
         await page.waitForNavigation({ waitUntil: ['networkidle2'] });
         await page.goto(url);
-    })();
+
+    } catch (err) {
+        console.log(err);
+    }
 }
